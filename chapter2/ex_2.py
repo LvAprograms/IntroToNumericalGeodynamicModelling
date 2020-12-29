83020c74357153c9c56602c6c18b8aacebffb818
Luuk van Agtmaal <luukv8x@hotmail.com>
2020-12-29 17:23:09 +0100

    made progress but density values are off, probably error in gibbs formula

--- chapter2/ex_2.py
+++ chapter2/ex_2.py
@@ -1,29 +1,92 @@
-from numpy import array, arange
+from numpy import zeros, arange, meshgrid, mean, min, max
 from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from chapter2.gibbsfree import Gibbs_m
+
 
 ## Exercise 2.2 (2.1 is analytical)
 T0 = 100    # deg K
 T1 = 4000   # deg K
 P0 = 1e9    # start of pressure interval, Pa
 P1 = 100e9  # End of pressure interval, Pa
-T = arange(T0, T1, 1)
-P = arange(P0, P1, 1E9)
+dT = 50      # Temperature step
+dP = 1e9    # Pressure step
+T = arange(T0, T1, dT)
+P = arange(P0, P1, dP)
+nT = len(T)-1
+nP = len(P) - 1
+m = 0.0403044 # molar mass of MgO, kg/mol
 
-R = 8.314 # gas constant, j/mol
-Pr = 1e5 # reference pressure, 1 bar
-Tr = 298.15 # reference surface temperature, K
+# R = 8.314 # gas constant, j/mol
+# Pr = 1e5 # reference pressure, 1 bar
+# Tr = 298.15 # reference plot_surfaceace temperature, K
+# #
+# # empirical parameters
+# Hr      = -6.015e5    # J empirical constant
+# Vr      = 1.12228e-5  # J/Pa
+# phi     = 3.01795e10 # Pa
+# c1      = 1.96612
+# c2      = 4.12756
+# c3      = 0.53690
+# deltaH1 = 2966.88 # J
+# deltaH2 = 56212.69 # J
+# deltaH3 = 27787.19 # J
+# deltaV1 = deltaV2 = 3.52971e-8 # J/Pa
+# deltaV3 = 1.9849568e-6 # J/Pa
 
-# empirical parameters
-Hr      = -6.015e5    # J empirical constant
-Vr      = 1.12228e-5  # J/Pa
-phi     = 3.01795e10 # Pa
-c1      = 1.96612 
-c2      = 4.12756
-c3      = 0.53690
-deltaH1 = 2966.88 # J
-deltaH2 = 56212.69 # J
-deltaH3 = 27787.19 # J
-deltaV1 = deltaV2 = 3.52971e-8 # J/Pa
-deltaV3 = 1.9849568e-6 # J/Pa
+
+def V(press, temp):
+    return (Gibbs_m(press + dP, temp) - Gibbs_m(press, temp)) / dP
+
+def rho(press, temp):
+    return m / V(press, temp)
+
+def alpha(press, temp):
+    return (-1/rho(press, temp)) * (rho(press, temp + dT) - rho(press, temp)) / dT
+
+def beta(press, temp):
+    return 1/rho(press,temp) * (rho(press + dP, temp) - rho(press, temp)) / dP
 
 
+
+# visualise
+fig, axes = plt.subplots(3,3, figsize=(20,12))
+volume = density = expansivity = compressibility = zeros((nT, nP))
+for i in range(nT):
+    for j in range(nP):
+        volume[i,j] = V(press=P[j], temp=T[i])
+        density[i,j] = rho(press=P[j], temp=T[i])
+        expansivity[i,j] = alpha(press=P[j], temp=T[i])
+        compressibility[i,j] = beta(press=P[j], temp=T[i])
+    print(T[i]) if i % 10 == 0 else None
+
+PP, TT = meshgrid(P[0:-1]/1e9, T[0:-1])
+
+# pcolormeshes
+for i in range(3):
+    if i == 0:
+        pc = axes[i][0].pcolormesh(TT, PP, density)
+        fig.colorbar(pc, ax=axes[i][0])
+        cf = axes[i][1].contourf(TT, PP, density)
+        ax = fig.add_subplot(3,3,i*3+3, projection='3d')
+        s = ax.plot_surface(TT, PP, density)
+    if i == 1:
+        pc = axes[i][0].pcolormesh(TT, PP, expansivity)
+        fig.colorbar(pc, ax=axes[i][0])
+        cf = axes[i][1].contourf(TT, PP, expansivity)
+        ax = fig.add_subplot(3,3,i*3+3, projection='3d')
+
+        s = ax.plot_surface(TT, PP, expansivity)
+    if i == 2:
+        pc = axes[i][0].pcolormesh(TT, PP, compressibility)
+        fig.colorbar(pc, ax=axes[i][0])
+        cf = axes[i][1].contourf(TT, PP, compressibility)
+        ax = fig.add_subplot(3,3,i*3+3, projection='3d')
+
+        s = ax.plot_surface(TT, PP, compressibility)
+        axes[i][0].set_xlabel("Temperature [K]")
+        axes[i][0].set_ylabel("Pressure [GPa]")
+    plt.pause(0.001)
+
+
+plt.show()
--- chapter2/gibbsfree.py
+++ chapter2/gibbsfree.py
@@ -16,7 +16,7 @@ def Gibbs_m(P, T):
     c2      = 4.12756
     c3      = 0.53690
     deltaH1 = 2966.88 # J
-    deltaH2 = 56212.69 # J
+    deltaH2 = 5621.69 # J
     deltaH3 = 27787.19 # J
     deltaV1 = deltaV2 = 3.52971e-8 # J/Pa
     deltaV3 = 1.9849568e-6 # J/Pa
@@ -27,8 +27,8 @@ def Gibbs_m(P, T):
     Psi = 0
     sumpart = 0
     for i in range(3):
-        Psi = (5/4) * (Pr + phi) **(1/5) * ((P + phi)**(4/5) - (Pr + phi)^(4/5))
+        Psi = (5/4) * (Pr + phi) **(1/5) * ((P + phi)**(4/5) - (Pr + phi)**(4/5))
         e = exp(-((dH[i] + dV[i] * Psi))/(R * T)) 
         eo = exp(-dH[i] / (R * Tr))
-        sumpart += c[i] * (R * T * log(1 - e) - dH[i] * eo / (1 - eo))
+        sumpart += c[i] * (R * T * log(1 - e) - (dH[i] * eo / (1 - eo)))
     return Hr + Vr * Psi + sumpart
