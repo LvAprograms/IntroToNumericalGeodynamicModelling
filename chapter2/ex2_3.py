c9b1aebc45d6350a17d08f9da150cd208c7c6aa2
Luuk van Agtmaal <luuk.vanagtmaal@erdw.ethz.ch>
2021-01-19 14:33:45 +0100

    Commented and improved efficiency of program

--- chapter2/ex2_3.py
+++ chapter2/ex2_3.py
@@ -2,14 +2,15 @@ from matplotlib import pyplot as plt
 from numpy import zeros, meshgrid
 
 
-nT, nP = 0, 0
-Tstart, Pstart, Tstep, Pstep = 0.0, 0.0, 0.0, 0.0
-T, P = zeros((nP, nT)), zeros((nP, nT))
-pyrolite = zeros((nP, nT))
-morb = zeros((nP, nT))
 
-def read_densities(file):
-    dens = zeros((1,1))
+# We have to read two files with the same format, so I define a function that reads and processes the data. 
+def read_densities(file, TP=False):
+    # initialise variables
+    dens = zeros((1,1)) 
+    nT, nP = 0, 0
+    Tstart, Pstart, Tstep, Pstep = 0.0, 0.0, 0.0, 0.0
+    T, P = zeros((nP, nT)), zeros((nP, nT))
+    # read the file and fill density matrix
     with open(file) as f:
         T, P = [], []
         for i, line in enumerate(f.readlines()):
@@ -24,15 +25,19 @@ def read_densities(file):
             elif i == 2: 
                 Pstep = float(l[0])
             elif i > 3:
-                dens[int((i-4)/nP), (i-4)%nT] = float(l[0])
-    print(Tstart, Pstart, Tstep, Pstep)
-    T = [Tstart + _ * Tstep for _ in range(nT)]
-    P = [Pstart + _ * Pstep for _ in range(nP)] 
-    return meshgrid(T, P), dens
+                dens[int((i-4)/nP), (i-4)%nT] = float(l[0]) # data starts at i = 4, each row in the matrix is nT long, so every nTth entry the row will increase. 
+    print("Tstart: {} K, Pstart: {} bar, Tstep: {} K, Pstep: {} bar".format(Tstart, Pstart, Tstep, Pstep))
+
+    if TP: # only calculate Temperature and Density vectors if not already done before
+        T = [Tstart + _ * Tstep for _ in range(nT)]
+        P = [Pstart + _ * Pstep for _ in range(nP)] 
+        return meshgrid(T, P), dens
+    else:
+        return dens 
                 
             
-[T, P], pyrolite = read_densities("chapter2/m895_ro")
-[T, P] , morb = read_densities("chapter2/morn_ro")
+[T, P], pyrolite = read_densities("chapter2/m895_ro", TP=True)
+morb = read_densities("chapter2/morn_ro")
 rhodiff = morb - pyrolite
 plt.figure()
 plt.contourf(T, P, rhodiff, cmap='hot')
